cfd6d2fc437f5619750cc9da6b47268145259af3
Elias-Dewa <elias.dewa19@gmail.com>
2023-05-22 07:43:57 -0700

    Unittests: task 4

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Unit tests for client"""
+
+
+import unittest
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Define a class"""
+    @parameterized.expand([
+        ("google"),
+        ("abc"),
+    ])
+    def test_org(self, input, mock_get):
+        """method that test GithubOrgClient.org and
+        returns the correct value"""
+        test_class = GithubOrgClient(input)
+        test_class.org()
+        mock_get.assert_called_once_with()
